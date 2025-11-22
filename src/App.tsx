bf43bb7892fd7498bd44d8b381655f5648dab4f2
yoshisuke1013 <shin10132022@gmail.com>
2025-11-22 13:26:26 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 
 function App() {
   return (
